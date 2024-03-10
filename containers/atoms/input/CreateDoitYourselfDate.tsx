b9266e0a0b5f7c50af478999251eed306bfc273d
Jinah <monsatfri@gmail.com>
2024-03-11 03:47:32 +0900

    💄 style: 할 일 모달 내 날짜 input 구현

---
+++ containers/atoms/input/CreateDoitYourselfDate.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import CreateDoItYourselfInput from "./CreateDoItYourselfInput";
+
+export default function CreateDoitYourselfDate() {
+  const calendarIcon = (
+    <Image
+      src="/assets/icon/calendarIcon.svg"
+      width={22}
+      height={22}
+      alt="pick date"
+    />
+  );
+  return (
+    <CreateDoItYourselfInput
+      title="마감일"
+      content="날짜를 입력해 주세요"
+      type="text"
+      icon={calendarIcon}
+    />
+  );
+}
