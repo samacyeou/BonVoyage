c58a461fbc1003a5ebc037537fe4e9a1844412a3
Jinah <monsatfri@gmail.com>
2024-03-11 03:46:40 +0900

    💄 style: 할 일 모달 내 댓글 textarea 구현

---
+++ containers/atoms/input/CreateDoitYourselfComment.tsx
@@ -0,0 +1,11 @@
+import CreateDoItYourselfInput from "./CreateDoItYourselfInput";
+
+export default function CreateDoItYourselfComment() {
+  return (
+    <CreateDoItYourselfInput
+      title="댓글"
+      content="댓글을 작성해 주세요"
+      type="textarea"
+    />
+  );
+}
