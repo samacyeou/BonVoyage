5095dad506dc834db8b6af6d3ca1709533ad4d9c
jiyong <parkjiy96@naver.com>
2024-03-11 14:17:17 +0900

    ♻️ refactor : src 타입 수정

--- src/@types/type.ts
+++ src/@types/type.ts
@@ -1,13 +1,19 @@
 //타입 들어갈곳
 
-
+import { StaticImageData } from 'next/image';
 export interface ButtonProps {
   name?: string;
   disabled?: boolean;
-  type?:string;
-  color?:string;
-  iconAlt?:string;
-  src?: string;
-  src2?: string;
+  type?: string;
+  color?: string;
+  iconAlt?: string;
+  src: StaticImageData
+  src2?: StaticImageData 
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
+
+//IntrinsicAttributes & Omit<DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>, "src" | ... 4 more ... | "loading"> & { ...; }
+//string | undefined' 형식은 'string | StaticImport' 형식에 할당할 수 없습니다.
+
+//src 속성의 타입은 string | StaticImport
+// src2
--- src/components/atoms/myDashboardBtn/index.tsx
+++ src/components/atoms/myDashboardBtn/index.tsx
@@ -7,9 +7,9 @@ const MyDashboard = ({ name, src, src2, iconAlt, onClick }: ButtonProps) => {
   return (
     <button className={styles.myDashboardBtn} onClick={onClick}>
       <div className={styles.container}>
-        {src && <Image src={src} alt={iconAlt} width={20} height={20} />}
+        <Image src={src} alt={iconAlt} width={10} height={10} />
         {name}
-        {src && <Image src={src2 ?? ""} alt={iconAlt} width={20} height={20} />}
+        <Image src={src2 ?? ""} alt={iconAlt} width={20} height={20} />
       </div>
       <div>
         <Image
@@ -26,8 +26,34 @@ const MyDashboard = ({ name, src, src2, iconAlt, onClick }: ButtonProps) => {
 
 export default MyDashboard;
 
+//src={src2 ?? ""}
+//src2가 null또는 undefinedd이면  ""로 대체
+
 //사용법
 
 //<MyDashboard name="비브리지" src={ellipseGreen} src2={crownIcon}/>
 
 //이런식으로 사용하실 컴포넌트에서 src, src2 prop에 이미지 import해서 사용하시면 됩니다.
+
+// const MyDashboard = ({ name, src, src2, iconAlt, onClick }: ButtonProps) => {
+//   return (
+//     <button className={styles.myDashboardBtn} onClick={onClick}>
+//       <div className={styles.container}>
+//         {src && <Image src={src} alt={iconAlt} width={10} height={10} />}
+//         {name}
+//         {src && <Image src={src2 ?? ""} alt={iconAlt} width={20} height={20} />}
+//       </div>
+//       <div>
+//         <Image
+//           src={forwardArrowIcon}
+//           alt="forwardArrowIcon"
+//           className={styles.arrowIcon}
+//           width={18}
+//           height={18}
+//         />
+//       </div>
+//     </button>
+//   );
+// };
+
+// export default MyDashboard;
