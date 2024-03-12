6f1bf1a3bb7d09d0e73f9d8361612edebfd37cab
jinyoung1018 <71465981+jinyoung1018@users.noreply.github.com>
2024-03-12 12:23:46 +0900

    [Feat] 사이드메뉴 제작 (#17)
    * feat: 사이드바 제작
    
    * feat: 사이드바 메뉴 prop 추가
    
    * chore: 사이드바 수정
    
    * chore: 테스트 주석 제거
    
    * chore: 충돌 해결
    
    * style: font-size 단위 수정
    
    ---------
    
    Co-authored-by: samacyeou <151595973+samacyeou@users.noreply.github.com>

---
+++ src/containers/atoms/sideBar/SideBar.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import styles from './sideBar.module.scss';
+import Image from 'next/image';
+import logo from '../../../../public/assets/icon/logo.svg';
+import taskify from '../../../../public/assets/icon/taskify.svg';
+import addBoxIcon from '../../../../public/assets/icon/addBoxIcon.svg';
+import SideBarMenu from '../sideBarMenu/SideBarMenu';
+
+export default function SideBar() {
+  return (
+    <div className={styles['sidebar']}>
+      <div className={styles['logoArea']}>
+        <Image src={logo}></Image>
+        <Image className={styles['logoText']} src={taskify}></Image>
+      </div>
+      <div className={styles['menuArea']}>
+        <div className={styles['menuTitleArea']}>
+          <h1 className={styles['menuTitle']}>Dash Boards</h1>
+          <Image src={addBoxIcon}></Image>
+        </div>
+        <SideBarMenu></SideBarMenu>
+      </div>
+    </div>
+  );
+}
