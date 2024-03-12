33e081d3a139eafb9ef09c4b85ecbcc528c0c1f1
jinyoung1018 <71465981+jinyoung1018@users.noreply.github.com>
2024-03-12 17:52:04 +0900

    [Feat] 로그인회원가입 input 컴포넌트 (#15)
    * feat: 이메일 input 퍼블리싱
    
    * feat: 이메일input 유효성 검사 기능 추가
    
    * fix: 글자 색상 변경
    
    * feat: 비밀번호 Input 퍼블리싱 및 유효성 검사
    
    * feat: 비밀번호 inputbox 기능 추가
    
    * 비밀번호 확인 input 퍼블리싱 및 기능 구현
    
    * 닉네임 input 퍼블리싱 및 기능 구현
    
    * fix: 테스트 코드 삭제
    
    * chore: package.json수정

---
+++ src/containers/atoms/input/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import styles from "./passwordInput.module.scss";
+import Image from "next/image";
+import eyeOffIcon from "../../../../../public/assets/icon/eyeOffIcon.svg";
+import eyeOnIcon from "../../../../../public/assets/icon/eyeOnIcon.svg";
+
+export default function PasswordInput() {
+  const [password, setPassword] = useState<string>("");
+  const [isValid, setIsValid] = useState<boolean>(true);
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const newPassword = e.target.value;
+    setPassword(newPassword);
+  };
+
+  const handleBlur = (): void => {
+    if (password.length < 8) {
+      setIsValid(false);
+    } else {
+      setIsValid(true);
+    }
+  };
+
+  const handleClick = (): void => {
+    setIsClicked(!isClicked);
+  };
+  return (
+    <div className={styles["inputForm"]}>
+      <label className={styles["inputLabel"]}>비밀번호</label>
+      <input
+        className={`${styles["inputBox"]} ${isValid ? "" : styles["invalid"]}`}
+        placeholder="비밀번호를 입력해 주세요"
+        type={isClicked ? "text" : "password"}
+        value={password}
+        onChange={handleInputChange}
+        onBlur={handleBlur}
+      ></input>
+      <Image
+        className={styles["eyeIcon"]}
+        onClick={handleClick}
+        src={isClicked ? eyeOnIcon : eyeOffIcon}
+        alt="EyeIcon"
+      ></Image>
+      {!isValid && (
+        <span className={styles["errorMsg"]}>8자 이상 입력해 주세요.</span>
+      )}
+    </div>
+  );
+}
