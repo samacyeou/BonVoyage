b6945c652ce5fe26341c541143cc086e9c0a0be4
김범주 <samacyeou999@gmail.com>
2024-03-12 19:11:17 +0900

    🚚 chore : 폴더명 정리

--- src/components/atoms/sideBarMenu/SideBarMenu.tsx
+++ src/components/atoms/sideBarMenu/SideBarMenu.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import styles from './sideBarMenu.module.scss';
-<<<<<<< HEAD
 import Image from 'next/image';
 import crownIcon from '../../../../public/assets/icon/crownIcon.svg';
 
@@ -16,19 +15,6 @@ export default function SideBarMenu({ menuTitle, path }: SideBarMenuProps) {
         <a className={styles['menuTitle']}>{menuTitle}</a>
       </Link>
       <Image className={styles['crownIcon']} src={crownIcon}></Image>
-=======
-
-interface SideBarMenuProps {
-  menuTitle?: string;
-}
-
-export default function SideBarMenu({ menuTitle }: SideBarMenuProps) {
-  return (
-    <div className={styles['sidebarMenu']}>
-      <Link href="/">
-        <a className={styles['menuTitle']}>{menuTitle}</a>
-      </Link>
->>>>>>> d961501 (🚚 chor : 폴더명 변경)
     </div>
   );
 }
