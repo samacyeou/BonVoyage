c55fdf6ed50aeef5961598f9bcdd0df49cf96763
jiyong <parkjiy96@naver.com>
2024-03-12 11:08:33 +0900

    🔧 chore : 필요없는 주석 삭제

--- src/containers/atoms/buttons/myDashboardBtn/index.tsx
+++ src/containers/atoms/buttons/myDashboardBtn/index.tsx
@@ -38,26 +38,4 @@ export default MyDashboard;
 //<MyDashboard name="비브리지" src={ellipseGreen} src2={crownIcon}/>
 
 //이런식으로 사용하실 컴포넌트에서 src, src2 prop에 이미지 import해서 사용하시면 됩니다.
-
-// const MyDashboard = ({ name, src, src2, iconAlt, onClick }: ButtonProps) => {
-//   return (
-//     <button className={styles.myDashboardBtn} onClick={onClick}>
-//       <div className={styles.container}>
-//         {src && <Image src={src} alt={iconAlt} width={10} height={10} />}
-//         {name}
-//         {src && <Image src={src2 ?? ""} alt={iconAlt} width={20} height={20} />}
-//       </div>
-//       <div>
-//         <Image
-//           src={forwardArrowIcon}
-//           alt="forwardArrowIcon"
-//           className={styles.arrowIcon}
-//           width={18}
-//           height={18}
-//         />
-//       </div>
-//     </button>
-//   );
-// };
-
-// export default MyDashboard;
+//src2, iconAlt은 선택사항입니다.
