ffaae2764ae712fb50c00d3ec24f8cdc38e9ac14
PARK JINAH <113954463+jinahp@users.noreply.github.com>
2024-03-13 11:23:42 +0900

    [Feat] 할 일 페이지 atoms 구현 (#18)
    * 💄 style: all: unset 속성에 textarea 추가
    
    * 💄 style: 할 일 모달 내 input 공통 컴포넌트 구현
    
    * 💄 style: 할 일 모달 내 제목 input 구현
    
    * 💄 style: 할 일 모달 내 댓글 textarea 구현
    
    * 💄 style: 할 일 모달 내 날짜 input 구현
    
    * 💄 style: 할 일 모달 내 태그 input 구현
    
    * 💄 style: 할 일 모달 내 설명 input 구현
    
    * ✨ feat: 할 일 모달 내 태그 input 중복 제한 및 7개 초과 제한 기능 설정
    
    ChipTag 컴포넌트 재사용으로 인해 CreateDoItYourselfTag 컴포넌트, css 파일을 atoms -> molecules 폴더로 이동 및 폴더구조 변경
    
    * ✨feat: 댓글 input, 날짜 input 구현 및 공통 컴포넌트 분리(input, textarea)
    
    폴더 구조 변경(containers->components) 및 프로젝트 설정 추가/변경(prettier, scss import type 설정)

---
+++ src/components/atoms/chipTag/ChipTag.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import styles from './chipTag.module.scss';
+import classNames from 'classnames/bind';
+
+const cn = classNames.bind(styles);
+
+interface Props {
+  tag: string;
+  color: string;
+  onClick?: () => void;
+}
+
+export default function ChipTag({ tag, color, onClick }: Props) {
+  return (
+    <div className={cn('tag', color)} onClick={onClick}>
+      <span>{tag}</span>
+      <Image
+        className={styles.closeIcon}
+        src='/assets/icon/closeIcon.svg'
+        alt='close Icon'
+        width={12}
+        height={12}
+      />
+    </div>
+  );
+}
---
+++ src/components/atoms/input/CreateDoItYourselfTitle.tsx
@@ -0,0 +1,11 @@
+import CreateDoItYourselfInput from './CreateDoItYourselfInput';
+
+export default function CreateDoItYourselfTitle() {
+  return (
+    <CreateDoItYourselfInput
+      title="제목"
+      content="제목을 입력해 주세요"
+      required
+    />
+  );
+}
