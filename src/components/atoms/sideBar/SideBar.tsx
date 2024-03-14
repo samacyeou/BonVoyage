42f688125387fdf707c292accb9f87034e5cfe69
jinyoung1018 <jywsb1816@gmail.com>
2024-03-14 18:33:31 +0900

    fix: 사이드바 로고 수정 및 여백 문제 해결

--- src/components/atoms/sideBar/SideBar.tsx
+++ src/components/atoms/sideBar/SideBar.tsx
@@ -1,26 +1,34 @@
 import styles from './sideBar.module.scss';
 import Image from 'next/image';
-import logo from '../../../../public/assets/icon/logo.svg';
-import taskify from '../../../../public/assets/icon/taskify.svg';
+// import logo from '../../../../public/assets/icon/logo.svg';
+// import taskify from '../../../../public/assets/icon/taskify.svg';
 import addBoxIcon from '../../../../public/assets/icon/addBoxIcon.svg';
 import SideBarMenu from '../sideBarMenu/SideBarMenu';
+import Link from 'next/link';
+import LogoWithTitle from '../logoWithTitle/LogoWithTitle';
 
-export default function SideBar() {
+interface prop {
+  path?: string;
+}
+export default function SideBar({ path }: prop) {
   return (
     <div className={styles['sidebar']}>
       <div className={styles['logoArea']}>
-        <img className={styles['logo']} src={'/assets/icon/logo.svg'}></img>
+        {/* <img className={styles['logo']} src={'/assets/icon/logo.svg'}></img>
         <img
           className={styles['logoText']}
           src={'/assets/icon/taskify.svg'}
-        ></img>
+        ></img> */}
+        <LogoWithTitle></LogoWithTitle>
       </div>
       <div className={styles['menuArea']}>
         <div className={styles['menuTitleArea']}>
           <h1 className={styles['menuTitle']}>Dash Boards</h1>
           <Image src={addBoxIcon}></Image>
         </div>
-        <SideBarMenu></SideBarMenu>
+        <Link href={`${path}`}>
+          <SideBarMenu menuTitle="코드잇" />
+        </Link>
       </div>
     </div>
   );
