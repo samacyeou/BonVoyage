f7482e8f6bc8736660a1369723e4d7054c0f9ee0
Jinah <monsatfri@gmail.com>
2024-03-15 14:50:00 +0900

    🐛 fix: 유효성 검사 오류 수정

--- src/components/atoms/input/common/CommonInput.tsx
+++ src/components/atoms/input/common/CommonInput.tsx
@@ -1,42 +1,53 @@
-import {
-  forwardRef,
-  HTMLAttributes,
-  HTMLInputTypeAttribute,
-  PropsWithChildren,
-  ReactNode,
-} from 'react';
 import classNames from 'classnames/bind';
-import { FieldErrors, UseFormRegisterReturn } from 'react-hook-form';
+import { HTMLInputTypeAttribute, PropsWithChildren, ReactNode } from 'react';
+import { FieldErrors, RegisterOptions, UseFormRegister } from 'react-hook-form';
 import styles from './commonInput.module.scss';
 
 const cn = classNames.bind(styles);
 
-export interface Props extends PropsWithChildren<UseFormRegisterReturn> {
+export interface CommonInputProps {
   errors: FieldErrors;
+  register: UseFormRegister<any>;
+  name?: string;
+  registerOptions?: RegisterOptions;
+}
+
+interface Props extends PropsWithChildren<CommonInputProps> {
   label: string;
+  name: string;
   placeholder: string;
   type: HTMLInputTypeAttribute;
   icon?: ReactNode;
 }
 
-const CommonInput = forwardRef<HTMLInputElement, Props>(function CommonInput(
-  { children, errors, label, icon, ...props },
-  ref,
-) {
-  const error = errors[props.name as string];
+function CommonInput({
+  children,
+  errors,
+  label,
+  name,
+  register,
+  registerOptions,
+  icon,
+  ...props
+}: Props) {
+  const error = errors[name as string];
   const invalid = error !== undefined;
 
   return (
     <div className={styles.inputForm}>
       <label className={styles.inputLabel}>{label}</label>
       {icon && <div className={styles.passwordIcon}>{icon}</div>}
-      <input className={cn('inputBox', { invalid })} {...props} ref={ref} />
+      <input
+        className={cn('inputBox', { invalid })}
+        {...register(name, registerOptions)}
+        {...props}
+      />
       {children}
       {invalid && (
         <span className={styles.errorMsg}>{error?.message as string}</span>
       )}
     </div>
   );
-});
+}
 
 export default CommonInput;
