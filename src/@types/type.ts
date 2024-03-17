058663cb2893b77ed9f369ab1880cfcb855ba1d5
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-03-17 13:13:27 +0900

    feat: MyHeader 닉네임 클릭에 의한 간단한 메뉴 추가
    * ✨ feat : 모바일 초대된 대시보드 목록 추가
    
    * ♻️ refactor : 초대된 대시보드 목록 컴포넌트화
    
    * ✨ feat : 무한 스크롤 기능 추가 중...
    
    * ✨ feat : 헤더 닉네임 메뉴 추가 중...
    
    * 💄 style : 이미지 인풋 스타일 수정
    
    * ✨ feat : 헤더 닉네임 클릭시 나오는 메뉴 추가

--- src/@types/type.ts
+++ src/@types/type.ts
@@ -64,7 +64,30 @@ export interface Dashboard {
   userId: number;
 }
 
-export interface ChangePasswordProps{
+// 초대된 대시보드
+export interface Invitation {
+  id: number;
+  inviter: {
+    nickname: string;
+    email: string;
+    id: number;
+  };
+  teamId: string;
+  dashboard: {
+    title: string;
+    id: number;
+  };
+  invitee: {
+    nickname: string;
+    email: string;
+    id: number;
+  };
+  inviteAccepted: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface ChangePasswordProps {
   password: string;
   newPassword: string;
 }
--- src/components/molecules/myHeader/MyHeader.tsx
+++ src/components/molecules/myHeader/MyHeader.tsx
@@ -1,9 +1,13 @@
-import HeaderBtn from '@/components/atoms/headerBtn/HeaderBtn';
 import styles from './myHeader.module.scss';
+import classNames from 'classnames/bind';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
 
 // import { useContext } from 'react';
 // import { userContext } from '@/pages/mypage/index';
+
+const cn = classNames.bind(styles);
 
 interface Props {
   profileImageUrl: string;
@@ -11,16 +15,45 @@ interface Props {
 }
 
 export default function MyHeader({ profileImageUrl, nickname }: Props) {
+  const [isOpenNicknameMenu, setIsOpenNicknameMenu] = useState(false);
+
   // const userInfo = useContext(userContext);
+
+  const onBlurName = () => {
+    setTimeout(() => setIsOpenNicknameMenu(false), 100);
+  };
+
   return (
-    <div className={styles['header']}>
-      <span className={styles['dashboardName']}>내 대시보드</span>
-      <div className={styles['management']}>
-        <div className={styles['user']}>
-          <div className={styles['profile']}>
+    <div className={cn('header')}>
+      <span className={cn('dashboardName')}>내 대시보드</span>
+      <div className={cn('management')}>
+        <div className={cn('user')}>
+          <div className={cn('profile')}>
             <Image layout="fill" src={profileImageUrl} alt="프로필 이미지" />
           </div>
-          <span>{nickname}</span>
+          <div className={cn('name')}>
+            <button
+              className={cn('nickname')}
+              onClick={() => setIsOpenNicknameMenu((preState) => !preState)}
+              onBlur={onBlurName}
+            >
+              {nickname}
+            </button>
+            {isOpenNicknameMenu && (
+              <div
+                className={cn('nicknameMenu')}
+                onBlur={() => setIsOpenNicknameMenu(false)}
+              >
+                <button className={cn('menuItem')}>
+                  <Link href="/myPage">마이페이지</Link>
+                </button>
+                <hr />
+                <button className={cn('menuItem', 'logout')}>
+                  <Link href="/">로그아웃</Link>
+                </button>
+              </div>
+            )}
+          </div>
           {/* <span>{userInfo.nickname}</span> */}
         </div>
       </div>
