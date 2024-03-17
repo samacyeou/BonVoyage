9151187fffd10f45ce1f9ad59e1e5bcd64669119
김범주 <samacyeou999@gmail.com>
2024-03-17 12:48:59 +0900

    ✨ feat : 헤더 닉네임 클릭시 나오는 메뉴 추가

--- src/components/molecules/myHeader/MyHeader.tsx
+++ src/components/molecules/myHeader/MyHeader.tsx
@@ -1,10 +1,13 @@
-import HeaderBtn from '@/components/atoms/headerBtn/HeaderBtn';
 import styles from './myHeader.module.scss';
+import classNames from 'classnames/bind';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useState } from 'react';
 
 // import { useContext } from 'react';
 // import { userContext } from '@/pages/mypage/index';
+
+const cn = classNames.bind(styles);
 
 interface Props {
   profileImageUrl: string;
@@ -15,33 +18,42 @@ export default function MyHeader({ profileImageUrl, nickname }: Props) {
   const [isOpenNicknameMenu, setIsOpenNicknameMenu] = useState(false);
 
   // const userInfo = useContext(userContext);
+
+  const onBlurName = () => {
+    setTimeout(() => setIsOpenNicknameMenu(false), 100);
+  };
+
   return (
-    <div className={styles['header']}>
-      <span className={styles['dashboardName']}>내 대시보드</span>
-      <div className={styles['management']}>
-        <div className={styles['user']}>
-          <div className={styles['profile']}>
+    <div className={cn('header')}>
+      <span className={cn('dashboardName')}>내 대시보드</span>
+      <div className={cn('management')}>
+        <div className={cn('user')}>
+          <div className={cn('profile')}>
             <Image layout="fill" src={profileImageUrl} alt="프로필 이미지" />
           </div>
-          <div className={styles['name']}>
-            <span
-              className={styles['nickname']}
+          <div className={cn('name')}>
+            <button
+              className={cn('nickname')}
               onClick={() => setIsOpenNicknameMenu((preState) => !preState)}
+              onBlur={onBlurName}
             >
               {nickname}
-            </span>
+            </button>
             {isOpenNicknameMenu && (
               <div
-                className={styles['nicknameMenu']}
+                className={cn('nicknameMenu')}
                 onBlur={() => setIsOpenNicknameMenu(false)}
               >
-                <button className={styles['MenuItem']}>마이페이지</button>
+                <button className={cn('menuItem')}>
+                  <Link href="/myPage">마이페이지</Link>
+                </button>
                 <hr />
-                <button className={styles['MenuItem']}>로그아웃</button>
+                <button className={cn('menuItem', 'logout')}>
+                  <Link href="/">로그아웃</Link>
+                </button>
               </div>
             )}
           </div>
-          <span>{nickname}</span>
           {/* <span>{userInfo.nickname}</span> */}
         </div>
       </div>
