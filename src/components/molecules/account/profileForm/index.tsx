82cb110a79cb56b7f83729d9a1bea2e52b1a06bc
jiyong <parkjiy96@naver.com>
2024-03-18 13:59:25 +0900

    ♻️ refactor : 프로필폼, 패스워드 폼 모달을 state에서 객체로 바꿈

--- src/components/molecules/account/profileForm/index.tsx
+++ src/components/molecules/account/profileForm/index.tsx
@@ -1,32 +1,42 @@
 import styles from './profileForm.module.scss';
 import ImageInput from '@/components/molecules/imageInput/ImageInput';
 import Button from '@/components/atoms/buttons/button';
-import { useState,useContext } from 'react';
+import { useState, useContext } from 'react';
 import { userContext } from '@/pages/mypage/index';
 import { userChangeAccount } from '@/api/accountApi/accountApi';
 import CommonInput from '@/components/atoms/input/common/CommonInput';
 import { useForm } from 'react-hook-form';
-import {UserChangeAccountProps} from '@/@types/type';
-
+import { UserChangeAccountProps } from '@/@types/type';
+import BaseModal from '@/components/atoms/baseModal/BaseModal';
 
 const ProfileForm = () => {
-  const { handleSubmit, register, reset } = useForm<UserChangeAccountProps>({ mode: 'all' }); 
+  const { handleSubmit, register, reset } = useForm<UserChangeAccountProps>({
+    mode: 'all',
+  });
   const userInfo = useContext(userContext);
   const [profileImageUrl, setProfileImageUrl] = useState<string>('');
+  const [modal, setModal] = useState({
+    isModalOpen: false,
+    modalMessage: '',
+  });
 
   const onSubmit = async (data: UserChangeAccountProps) => {
     try {
       await userChangeAccount({
         nickname: data.nickname,
-        profileImageUrl,
+        profileImageUrl: profileImageUrl,
       });
-      reset()
+
+      setModal({ isModalOpen: true, modalMessage: '프로필이 변경되었습니다.' });
+      reset();
       setProfileImageUrl('');
     } catch (error) {
       console.error('닉네임 또는 프로필 이미지 변경 실패:', error);
     }
   };
-
+  const closeModal = () => {
+    setModal({ isModalOpen: false, modalMessage: '' });
+  };
   return (
     <div className={styles.container}>
       <h1>프로필</h1>
@@ -56,6 +66,21 @@ const ProfileForm = () => {
             color="blue"
             onClick={handleSubmit(onSubmit)}
           />
+          {modal.isModalOpen && (
+            <BaseModal closeModal={closeModal}>
+              <div className={styles.modalContainer}>
+                <p>{modal.modalMessage}</p>
+                <div className={styles.modalBtn}>
+                  <Button
+                    name="확인"
+                    type="modal"
+                    color="blue"
+                    onClick={closeModal}
+                  />
+                </div>
+              </div>
+            </BaseModal>
+          )}
         </div>
       </form>
     </div>
