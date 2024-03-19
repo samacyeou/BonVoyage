950ed7b3605aab97749ea1d3d8214158d1276a38
jiyong <parkjiy96@naver.com>
2024-03-19 20:09:15 +0900

    ✨ feat :  로그인이 되어있으면 랜딩페이지 못가게 하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,20 @@ import HeaderAuth from '@/components/molecules/header/headerAuth/HeaderAuth';
 import LandingHeader from '@/components/molecules/landing/landingHeader';
 import LandingMain from '@/components/molecules/landing/landingMain';
 import LandingFooter from '@/components/molecules/landing/landingFooter';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const index = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('accessToken');
+    if (token) {
+      router.push('/mydashboard');
+    }
+  }, []);
+  // 로그인 되어있으면 mydashboard로 이동
+
   return (
     <div className={styles.container}>
       <HeaderAuth />
