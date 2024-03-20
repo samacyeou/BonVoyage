5a2ff0ddb6106b3d94480b3f956853d3e6e59ce0
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-03-20 11:16:20 +0900

    refactor: MyHeader, HeaderMyDashboard 통합
    * ♻️ refactor : 컨텍스토 롤백
    
    * 💄 style : MyHeader를 HeaderMyDashboard에 통합

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
-import { DashboardListProvider } from '@/contexts/DashboardListContext';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <DashboardListProvider>
-        <div id="modal" />
-        <Component {...pageProps} />
-      </DashboardListProvider>
+      <div id="modal" />
+      <Component {...pageProps} />
     </>
   );
 }
