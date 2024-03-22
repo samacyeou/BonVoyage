60c9cee4b58d16e27b9f727ed08261ac2e242c1d
jihee <sweet9811@gmail.com>
2024-03-22 16:47:34 +0900

    ♻️ refactor: 헤더 컴포넌트 통합

--- src/components/molecules/layout/index.tsx
+++ src/components/molecules/layout/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import SideBar from '@/components/atoms/sideBar/SideBar';
 import styles from './layout.module.scss';
-import MyHeader from '../myHeader/MyHeader';
 import HeaderMyDashboard from '@/components/molecules/header/headerMyDashboard/HeaderMyDashboard';
 import { useContext } from 'react';
 import { userContext } from '@/pages/mypage/index';
@@ -11,16 +10,19 @@ const Layout = () => {
 
   return (
     <div className={styles.container}>
-      <MyHeader
+      {/* <MyHeader
         title="계정관리"
         nickname={userInfo.nickname}
         profileImageUrl={userInfo.profileImageUrl}
+      /> */}
+      <HeaderMyDashboard
+        profile={userInfo.profileImageUrl}
+        boardTitle={'계정관리'}
+        name={userInfo.nickname}
       />
-      {/* <HeaderMyDashboard enName={userInfo.profileImageUrl} boardTitle={"계정관리"} koName={userInfo.nickname}/> */}
       <SideBar />
     </div>
   );
 };
 
 export default Layout;
-
