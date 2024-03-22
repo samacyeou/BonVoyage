742fde7505613527811172cd48f9bea2291aad18
jihee <sweet9811@gmail.com>
2024-03-23 08:15:03 +0900

    ♻️ refactor: colorPalette컴포넌트 적용 및 유지보수 용이하도록 파일분리

---
+++ src/styles/colorList.tsx
@@ -0,0 +1,9 @@
+export const COLOR_LIST: { [value: string]: string } = {
+  green: '#7AC555',
+  orange: '#FFA500',
+  pink: '#E876EA',
+  purple: '#760DDE',
+  blue: '#76A5EA',
+};
+
+export const COLOR_NAMES = ['green', 'purple', 'orange', 'pink', 'blue'];
