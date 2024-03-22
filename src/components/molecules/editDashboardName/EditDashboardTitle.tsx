e0ec2df497450c7be005652eb04e9a42ef0b9330
jihee <sweet9811@gmail.com>
2024-03-23 08:15:03 +0900

    ♻️ refactor: colorPalette컴포넌트 적용 및 유지보수 용이하도록 파일분리

--- src/components/molecules/editDashboardName/EditDashboardTitle.tsx
+++ src/components/molecules/editDashboardName/EditDashboardTitle.tsx
@@ -1,14 +1,49 @@
 import Link from 'next/link';
 import styles from './editDashboardTitle.module.scss';
-import BigColorDot from '@/components/atoms/colorDot/BigColorDot';
 import Button from '@/components/atoms/buttons/button';
+import ColorPalette from '../colorPalette/ColorPalette';
+import { useEffect, useState } from 'react';
+import { COLOR_LIST, COLOR_NAMES } from '@/styles/colorList';
+import axios from '@/api/axios';
+import { useRouter } from 'next/router';
 
-export default function EditDashboardTitle({ boardTitle = '내 대시보드' }) {
+export default function EditDashboardTitle() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const [dashboardInfo, setDashboardInfo] = useState();
+  console.log(id);
+
+  const onClickPaletteColor = (color: string) => {
+    setDashboardInfo((preData) => ({
+      ...preData,
+      color: COLOR_LIST[color],
+    }));
+  };
+
+  async function getDashboard(targetId: string) {
+    const res = await axios.get(`/dashboards/${targetId}`);
+    const nextDashboard = res.data;
+    setDashboardInfo(nextDashboard);
+    console.log(targetId);
+  }
+
+  useEffect(() => {
+    if (!id) return;
+    getDashboard(id);
+  }, [id]);
+
+  if (!dashboardInfo) return null;
+
   return (
     <div className={styles['container']}>
       <div className={styles['titleAndColor']}>
-        <span>{boardTitle}</span>
-        <BigColorDot colorName="orange" size={24} check={true} />
+        <span>{dashboardInfo.title}</span>
+        <ColorPalette
+          colorList={COLOR_LIST}
+          colorNameList={COLOR_NAMES}
+          // onClickPaletteColor={onClickPaletteColor}
+        />
       </div>
       <div className={styles['changeTitle']}>
         <span>대시보드 이름</span>
